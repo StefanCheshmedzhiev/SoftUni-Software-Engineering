678053e6e7cbf81e9606771ab858fc11e51e0c5a
StefanCheshmedzhiev <s.cheshmedzhiev@abv.bg>
2021-03-01 18:59:07 +0200

    Change double to var

--- CSharpProgrammingBasics/SimpleCalculations/CircleAreaAndPerimeter/Program.cs
+++ CSharpProgrammingBasics/SimpleCalculations/CircleAreaAndPerimeter/Program.cs
@@ -10,9 +10,9 @@ namespace CircleAreaAndPerimeter
     {
         static void Main(string[] args)
         {
-            double r = double.Parse(Console.ReadLine());
-            double area = Math.PI * r * r;
-            double perimeter = 2 * Math.PI * r;
+            var r = double.Parse(Console.ReadLine());
+            var area = Math.PI * r * r;
+            var perimeter = 2 * Math.PI * r;
             Console.WriteLine(area);
             Console.WriteLine(perimeter);
         }
